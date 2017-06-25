f77499f127e0a82f5dffe2585c2d0e6263e10d5b
c9411010 <626474@gmail.com>
2017-06-25 08:39:56 -0400

    Add project files.

---
+++ SouthmillReportingDataLayer/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data;
+
+namespace SouthmillReportingDataLayer
+{
+    public static class datalayer
+    {
+        public static DataSet getData()
+        {
+            
+            // Create a Database object from the factory using the connection string name.
+            Database db = utilityDataLayerFunctions.getConnectionDatabase();
+            // Using a SQL statement.
+            string sql = "daily_receiving_report";
+            var productDataSet = db.ExecuteDataSet(CommandType.StoredProcedure, sql);
+            return productDataSet;
+            
+        }
+
+        public static DataSet getInternalFarms()
+        {
+
+            // Create a Database object from the factory using the connection string name.
+            Database db = utilityDataLayerFunctions.getConnectionDatabase();
+            // Using a SQL statement.
+            string sql = "getFarms";
+            var productDataSet = db.ExecuteDataSet(CommandType.StoredProcedure, sql);
+            return productDataSet;
+
+        }
+    }
+}
---
+++ SouthmillReportingDataLayer/utilityDataLayerFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data;
+
+namespace SouthmillReportingDataLayer
+{
+    public static  class utilityDataLayerFunctions
+    {
+        public static Database getConnectionDatabase()
+        {
+        
+            DatabaseProviderFactory factory = new DatabaseProviderFactory();
+        // Create a Database object from the factory using the connection string name.
+            Database db = factory.Create("DefaultConnection");
+            return db;
+        }
+
+    }
+}
